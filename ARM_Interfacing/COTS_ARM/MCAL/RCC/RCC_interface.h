3459882a59cf916c1579318174d0c67b4ae418aa
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-05-04 21:31:59 +0300

    Update ARM COTS

---
+++ ARM_Interfacing/COTS_ARM/MCAL/RCC/RCC_interface.h
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------------------------
+ * Filename     :	RCC_interface.h
+ *
+ * Author       :	Ahmed Essam El-Mogy
+ * Created on   :	MAY 1, 2023
+ * Version      :	v1.0
+ * C Standard   :	C99
+ * Compiler     :	GNU ARM GCC
+ * Controller   :   STM32F401CCU6 (32-bit Architecture)
+ * Layer        :   MCAL
+ * ----------------------------------------------------------------------------------
+ *
+ * ----------------------------------------------------------------------------------
+ * Version		Date 		        Author			        Description
+ * v1.0		    MAY 1, 2023 		Ahmed Essam El-Mogy		Initial Creation
+ * ----------------------------------------------------------------------------------
+ */
+#ifndef RCC_INTERFACE_H
+#define RCC_INTERFACE_H
+
+
+
+#endif /* RCC_INTERFACE_H */
---
+++ ARM_Interfacing/COTS_ARM/MCAL/RCC/RCC_program.c
@@ -0,0 +1,41 @@
+/*
+ * ----------------------------------------------------------------------------------
+ * Filename     :	RCC_program.c
+ *
+ * Author       :	Ahmed Essam El-Mogy
+ * Created on   :	MAY 1, 2023
+ * Version      :	v1.0
+ * C Standard   :	C99
+ * Compiler     :	GNU ARM GCC
+ * Controller   :   STM32F401CCU6 (32-bit Architecture)
+ * Layer        :   MCAL
+ * ----------------------------------------------------------------------------------
+ *
+ * ----------------------------------------------------------------------------------
+ * Version		Date 		        Author			        Description
+ * v1.0		    MAY 1, 2023 		Ahmed Essam El-Mogy		Initial Creation
+ * ----------------------------------------------------------------------------------
+ */
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "RCC_interface.h"
+#include "RCC_private.h"
+#include "RCC_config.h"
+
+
+void MRCC_voidInit(void)
+{
+    
+}
+
+void MRCC_voidEnablePeripheralClock(u8 Copy_u8PeripheralBus, u8 Copy_u8PeripheralID)
+{
+
+}
+
+void MRCC_voidDisablePeripheralClock(u8 Copy_u8PeripheralBus, u8 Copy_u8PeripheralID)
+{
+
+}
