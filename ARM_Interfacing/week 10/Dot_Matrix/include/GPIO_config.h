0c6cb11f2f17e0548d1011e37fabdd879fb42b15
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-05-11 23:21:01 +0300

    Adding Dot Matrix Application

---
+++ ARM_Interfacing/week 10/Dot_Matrix/include/GPIO_config.h
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------------------------
+ * Filename     :	GPIO_config.h
+ *
+ * Author       :	Ahmed Essam El-Mogy
+ * Created on   :	MAY 5, 2023
+ * Version      :	v1.0
+ * C Standard   :	C99
+ * Compiler     :	GNU ARM GCC
+ * Controller   :   STM32F401CCU6 (32-bit Architecture)
+ * Layer        :   MCAL
+ * ----------------------------------------------------------------------------------
+ *
+ * ----------------------------------------------------------------------------------
+ * Version		Date 		        Author			        Description
+ * v1.0		    MAY 5, 2023 		Ahmed Essam El-Mogy		Initial Creation
+ * ----------------------------------------------------------------------------------
+ */
+#ifndef GPIO_CONFIG_H
+#define GPIO_CONFIG_H
+
+
+
+#endif /* GPIO_CONFIG_H */
